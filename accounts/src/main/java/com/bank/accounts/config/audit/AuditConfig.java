1ea825393cdbc11de26a2732ec7ef2b936160e6c
Marcus Rangel <marcus.vbrangel@gmail.com>
2026-01-29 19:58:33 -0300

    feat: implemented the first part of openapi docs on accounts microservice

--- accounts/src/main/java/com/bank/accounts/config/audit/AuditConfig.java
+++ accounts/src/main/java/com/bank/accounts/config/audit/AuditConfig.java
@@ -1,4 +1,9 @@
 package com.bank.accounts.config.audit;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class AuditConfig {
 }
