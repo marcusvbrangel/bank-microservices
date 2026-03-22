d190fe327692677bfcea530033467cce53f322ca
Marcus Rangel <marcus.vbrangel@gmail.com>
2026-03-22 14:51:19 -0300

    feat: implemented eureka client on all of microservices

---
+++ eurekaserver/src/main/java/com/bank/eurekaserver/config/eureka/EurekaServerCofiguration.java
@@ -0,0 +1,9 @@
+package com.bank.eurekaserver.config.eureka;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class EurekaServerCofiguration {
+}
